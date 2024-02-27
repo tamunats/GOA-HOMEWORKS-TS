6c143c7acab674198a9d463a1b42b6cbef0c20ce
tamunats <tsitlidze.tamar05@gmail.com>
2024-02-28 00:55:13 +0400

    second commit

---
+++ day5/classwork/lesson5.py
@@ -0,0 +1,16 @@
+#declaring value of first price
+first_price = 1000
+
+#declaring a variable of discount
+discount = 10
+
+#declaring value of price after discount
+price_after_discount1 = first_price - first_price * discount / 100
+
+discount = 20
+
+price_after_discount2 = first_price - first_price * discount / 100
+
+#displaying last price
+print(price_after_discount1)
+print(price_after_discount2)
---
+++ day5/homework/lesson5.py
@@ -0,0 +1,57 @@
+# declaring variables for the book names
+
+book1_name = "red and black"
+book2_name = "the catcher in the rye"
+book3_name = "parmis savane"
+book4_name = "mze mtvare da puris yana"
+book5_name = "brilka"
+book6_name = "me bebia iliko da olarioni"
+book7_name = "heladosi"
+book8_name = "amaoebis bazari"
+book9_name = "citeli sastumro"
+book10_name = "benjamin batonis ucnauri ambavi"
+
+#declaring value of book price
+price_book1 = 20
+price_book2 = 30
+price_book3 = 40
+price_book4 = 50
+price_book5 = 60
+price_book6 = 70
+price_book7 = 80
+price_book8 = 90
+price_book9 = 100
+price_book10 = 120
+
+#declaring a variable of discount in percent
+discount1 = 10
+discount2 = 20
+
+#declaring value of price after discount
+discount_price_after_discount_book1 = price_book1 - price_book1 * discount1 / 100
+discount_price_after_discount_book2 = price_book2 - price_book2 * discount1 / 100
+discount_price_after_discount_book3 = price_book3 - price_book3 * discount1 / 100
+discount_price_after_discount_book4 = price_book4 - price_book4 * discount1 / 100
+discount_price_after_discount_book5 = price_book5 - price_book5 * discount1 / 100
+discount_price_after_discount_book6 = price_book6 - price_book6 * discount2 / 100
+discount_price_after_discount_book7 = price_book7 - price_book7 * discount2 / 100
+discount_price_after_discount_book8 = price_book8 - price_book8 * discount2 / 100
+discount_price_after_discount_book9 = price_book9 - price_book9 * discount2 / 100
+discount_price_after_discount_book10= price_book10 - price_book10 * discount2 / 100
+
+#print book name and price after discount
+
+print(book1_name, discount_price_after_discount_book1)
+print(book2_name, discount_price_after_discount_book2)
+print(book3_name, discount_price_after_discount_book3)
+print(book4_name, discount_price_after_discount_book4)
+print(book5_name, discount_price_after_discount_book5)
+print(book6_name, discount_price_after_discount_book6)
+print(book7_name, discount_price_after_discount_book7)
+print(book8_name, discount_price_after_discount_book8)
+print(book9_name, discount_price_after_discount_book9)
+print(book10_name, discount_price_after_discount_book10)
+
+
+
+
