121e2a8371846440a02a61ae4801ae29e15f60a0
tamunats <tsitlidze.tamar05@gmail.com>
2024-03-07 19:38:45 +0400

    tenth commit

---
+++ day10/classwork/lasswork10.py
@@ -0,0 +1,12 @@
+#for i in range(3):
+#  print(i)
+
+#for num in range(5, 20): # გამოიტანს 5 დან 19 ის ჩათვლით
+#    print(num)
+
+for num in range(5,20):
+    print(num)
+
+print(range(5, 20))    
+
+print("Tamo"[0])
